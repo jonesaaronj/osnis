b6d8205bb849c91e4e7365f3c20f44d86ef9a61c
Aaron Jones <aaron.jones@accretivetg.com>
2018-08-01 12:28:08 -0700

    stuff

--- src/disc_info.c
+++ src/disc_info.c
@@ -104,7 +104,7 @@ struct DiscInfo * profileImage(char *file)
 void getDiscInfo(struct DiscInfo *discInfo, unsigned char data[])
 {
 	// check for the shrunken magic word
-    bool isShrunken = memcmp(SHRUNKEN_MAGIC_WORD, data, 6) == 0 || discInfo->isShrunken;
+    bool isShrunken = memcmp(SHRUNKEN_MAGIC_WORD, data, 6) == 0;
 
     // if this is a shrunken disc image the first block
     // is the partition table and the second block has all the
@@ -151,7 +151,7 @@ void getDiscInfo(struct DiscInfo *discInfo, unsigned char data[])
             return;
         }
 
-        if (!isShrunken) {
+        if (discInfo->table == null) {
             // create a partition table
             discInfo->table = calloc(1, BLOCK_SIZE);
 
